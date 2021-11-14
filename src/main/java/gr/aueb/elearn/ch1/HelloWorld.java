e74181531c2784b5070857c1731e68b2d8580c74
kvavoulis <kostas.vavoulis@gmail.com>
2021-11-14 19:15:53 +0200

    Hello World v 0.2

--- src/main/java/gr/aueb/elearn/ch1/HelloWorld.java
+++ src/main/java/gr/aueb/elearn/ch1/HelloWorld.java
@@ -1,5 +1,10 @@
 package gr.aueb.elearn.ch1;
 
+/*
+ * Prints Hello World
+ * @author Vavoulis C.
+ */
+
 public class HelloWorld {
     public static  void main(String[] args) {
         System.out.println("Hello World");
